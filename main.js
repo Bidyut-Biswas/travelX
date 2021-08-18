fbe5825153f7a7bd825a091ffe6f700c4f1d0b0e
bidyut148 <74338515+bidyut148@users.noreply.github.com>
2021-08-18 18:19:41 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+function handleQuantity(planet,operator){
+    const quantity=document.getElementById(`${planet}-input`);
+    let quantityNumber=parseInt(quantity.value)
+    if(operator==='plus'){
+        quantityNumber++;        
+    }else{
+        if(quantityNumber<1){
+            return;
+        }
+        quantityNumber--;
+        
+    }
+    quantity.value=quantityNumber
+    calculation(planet);
+    totalCost()
+}
+
+function calculation(planet){
+    const quantity=document.getElementById(`${planet}-input`);
+    let quantityNumber=parseInt(quantity.value);
+    
+    if(planet=='mars'){
+        let marsTotal=quantityNumber*500;
+        document.getElementById(`${planet}-span`).innerText=marsTotal
+    }else{
+        let moonTotal=quantityNumber*200;
+        document.getElementById(`${planet}-span`).innerText=moonTotal
+    }
+    
+}
+
+function totalCost(){
+    const marsQuantity=document.getElementById('mars-input');
+    let marsNumber=parseInt(marsQuantity.value);
+
+    const moonQuantity=document.getElementById('moon-input');   
+    let moonNumber=parseInt(moonQuantity.value);
+    document.getElementById('total-cost').innerText=marsNumber*5000+moonNumber*200
+}
+
+
